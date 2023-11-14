62909d5822e2a30b516d7a3a1299b5ffd7f7a04a
Coline Dusserre <coline.dusserre@kedgebs.com>
2023-11-14 18:17:50 +0100

    create filter method

--- app/Http/Controllers/WebscrapeController.php
+++ app/Http/Controllers/WebscrapeController.php
@@ -8,6 +8,8 @@ use Symfony\Component\DomCrawler\Crawler;
 class WebscrapeController extends Controller
 {
     private $results = array();
+    private$longTitleEntries = array();
+    private $smallTitleEntries = array();
     public function scraper()
     {
         $client = new Client();
@@ -23,28 +25,67 @@ class WebscrapeController extends Controller
                     return false;
                 }
                 $counter++;
-                $nextElement = $entry->nextAll()->filter('tr')->first();
-
-                if ($nextElement->filter('td.subtext span.subline')->count() > 0) {
-                    $comments = $nextElement->filter('td.subtext span.subline a:contains("comments"), a:contains("discuss"), a:contains("comment")')->text();
-                    $points = $nextElement->filter('td.subtext span.score')->text();
-                } else {
-                    $comments = '';
-                    $points = 0;
-                }
-                array_push($this->results, [
-                    'rank' => $entry->filter('td.title span.rank')->text(),
-                    'title' => $entry->filter('td.title span.titleline')->text(),
-                    'points' => $points,
-                    'comments' => $comments,
-                ]
-                );
+                $this->scrapeInfo($entry);
         });
+
         return response()->json(
             [
                 'entries' => $this->results,
                 'status' => $html->getStatusCode(),
             ]
         );
+    }
+
+    private function filterResults(string $title, string $rank, string $numberOfComments, string $points ) {
+        $wordsInTitle = str_word_count($title, 0);
+        //Filter all previous entries with more than five words in the title ordered by the number of comments first.
+        if ($wordsInTitle > 5) {
+            array_push($this->longTitleEntries, [
+                'rank' => $rank,
+                'title' => $title,
+                'points' => $points,
+                'comments' => $numberOfComments,
+                ]
+            );
+            //desc order for comments
+            usort($this->longTitleEntries, function ($a, $b) {
+                return $b['comments'] <=> $a['comments'];
+            });
+        } else {
+        // Filter all previous entries with less than or equal to five words in the title ordered by points.
+            array_push($this->smallTitleEntries, [
+                'rank' => $rank,
+                'title' => $title,
+                'points' => $points,
+                'comments' => $numberOfComments,
+                ]
+            ); 
+            //desc order for points
+            usort($this->smallTitleEntries, function ($a, $b) {
+                return $b['points'] <=> $a['points'];
+            });
+        } 
+
+        $this->results = array_merge($this->longTitleEntries, $this->smallTitleEntries);
+        return $this->results;
+    }
+
+    private function scrapeInfo(Crawler $entry) {
+        $nextElement = $entry->nextAll()->filter('tr')->first();
+        $title = $entry->filter('td.title span.titleline')->text();
+        $rank = $entry->filter('td.title span.rank')->text();
+
+        if ($nextElement->filter('td.subtext span.subline')->count() > 0) {
+            $numberOfComments = filter_var($nextElement->filter('td.subtext span.subline a:contains("comments"), a:contains("discuss"), a:contains("comment")')->text(), FILTER_SANITIZE_NUMBER_INT);
+            $points = filter_var($nextElement->filter('td.subtext span.score')->text(), FILTER_SANITIZE_NUMBER_INT);
+        } else {
+            $numberOfComments = 0;
+            $points = 0;
+        }
+
+        //apply filters
+        $this->filterResults($title, $rank, $numberOfComments, $points );
+
+        return $this->results;
     }
 }
