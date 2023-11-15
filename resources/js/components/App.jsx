b6fd39fde328f266fca621e477bdea1f3a152272
Coline Dusserre <coline.dusserre@kedgebs.com>
2023-11-15 14:42:07 +0100

    show properly list items

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,23 +1,35 @@
+import * as React from "react";
 import { useEffect, useState } from "react"
 
 export default function App() {
-    const [entries, setEntries] = useState()
+    const [entries, setEntries] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
     const initialize = async () => {
-        const data = await (await fetch('/scraper')).json();
-        if (data.status === 200) {
-            setEntries(data.entries);
-        } else {
-            console.error('error');
-        }
+        try {
+            const data = await (await fetch('/scraper')).json();
+            if (data.status === 200) {
+                setEntries(data.entries);
+            } else {
+                setErrorMessage(data.error);
+            }
+        } catch (error) { console.log(error) }
     }
 
     useEffect(() => {
-        initialize()
+        initialize();
     }, [])
 
     return (
-        entries && entries.map((e, index) =>
-            <ul key={index}><div>{e.rank}- {e.title}</div><div>{e.points} points, {e.comments} comment(s)</div></ul>
-        )
+        <><h1>30 first entries</h1>
+            {entries.length === 0 && errorMessage.length === 0 && <p>... scraping data</p>}
+            {errorMessage && <p>{errorMessage}</p>}
+            <ul>
+                {entries && entries.map((e, index) => <li key={index}>
+                    <div>{e.rank}- {e.title}</div>
+                    <div>{e.points} points, {e.comments} comment(s)</div>
+                </li>
+                )}
+            </ul>
+        </>
     );
 }
